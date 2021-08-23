747aef36b488d8508743431dad3412771578e99f
Joydeep <joydeep.chowdhury@oracle.com>
2021-08-23 10:43:17 +0530

    First commit

---
+++ src/test/java/joydeep/springframework/spring/framework/pet/clinic/SpringFrameworkPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package joydeep.springframework.spring.framework.pet.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
