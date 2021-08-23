b37a4a02d1e41e00bfd3013bd33161c3e2297d2d
Joydeep <joydeep.chowdhury@oracle.com>
2021-08-23 16:56:49 +0530

    Implement Pet clinic POJO data model Closes #8 .

---
+++ src/main/java/joydeep/springframework/spring/framework/pet/clinic/models/Owner.java
@@ -0,0 +1,4 @@
+package joydeep.springframework.spring.framework.pet.clinic.models;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/joydeep/springframework/spring/framework/pet/clinic/models/PetType.java
@@ -0,0 +1,13 @@
+package joydeep.springframework.spring.framework.pet.clinic.models;
+
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
