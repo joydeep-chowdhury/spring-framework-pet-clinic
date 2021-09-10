bad80639be676b1c8a7b81900e61f00bc854863b
Joydeep <joydeep.chowdhury@oracle.com>
2021-09-10 18:11:32 +0530

    Create vets index page and controller closes #13

---
+++ spring-framework-pet-clinic-web/src/main/java/joydeep/springframework/spring/framework/pet/clinic/models/controllers/VetsController.java
@@ -0,0 +1,13 @@
+package joydeep.springframework.spring.framework.pet.clinic.models.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetsController {
+
+    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
+    public String index(){
+        return "vets/index";
+    }
+}
