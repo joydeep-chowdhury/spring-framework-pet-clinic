76bf2dfbe83b9ef649c02528526d6acf20afb402
Joydeep <joydeep.chowdhury@oracle.com>
2021-09-12 00:26:25 +0530

    Display Vets in vets index page closes #18

--- spring-framework-pet-clinic-web/src/main/java/joydeep/springframework/spring/framework/pet/clinic/controllers/VetsController.java
+++ spring-framework-pet-clinic-web/src/main/java/joydeep/springframework/spring/framework/pet/clinic/controllers/VetsController.java
@@ -1,13 +1,24 @@
 package joydeep.springframework.spring.framework.pet.clinic.controllers;
 
+import joydeep.springframework.spring.framework.pet.clinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetsController {
 
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String index(){
-         return "vets/index";
+    public String index(Model model){
+        model.addAttribute("vets",vetService.findAll());
+
+        return "vets/index";
     }
 }
