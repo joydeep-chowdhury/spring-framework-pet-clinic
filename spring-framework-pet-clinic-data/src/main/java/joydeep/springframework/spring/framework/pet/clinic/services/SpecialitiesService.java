1f7e55000d187a308b84eef5763d5f0341f6fa9f
Joydeep <joydeep.chowdhury@oracle.com>
2021-09-15 11:53:57 +0530

    Create Speciality Map Service closes #42

---
+++ spring-framework-pet-clinic-data/src/main/java/joydeep/springframework/spring/framework/pet/clinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package joydeep.springframework.spring.framework.pet.clinic.services;
+
+import joydeep.springframework.spring.framework.pet.clinic.models.Speciality;
+
+public interface SpecialitiesService extends CrudRepository<Speciality, Long>{
+}
---
+++ spring-framework-pet-clinic-data/src/main/java/joydeep/springframework/spring/framework/pet/clinic/services/map/SpecialityMapService.java
@@ -0,0 +1,35 @@
+package joydeep.springframework.spring.framework.pet.clinic.services.map;
+
+import joydeep.springframework.spring.framework.pet.clinic.models.Speciality;
+import joydeep.springframework.spring.framework.pet.clinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Speciality save(Speciality entity) {
+        return super.save(entity);
+    }
+
+    @Override
+    public Speciality findByIdentity(Long id) {
+        return super.findByIdentity(id);
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteByIdentity(Long id) {
+        super.deleteByIdentity(id);
+    }
+
+    @Override
+    public void delete(Speciality entity) {
+        super.delete(entity);
+    }
+}
