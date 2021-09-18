b585ab741e01bc3d11af9f7d8aa68ceb36862f13
Joydeep <joydeep.chowdhury@oracle.com>
2021-09-18 20:25:30 +0530

    Convert Owners to JPA Entities closes #45

--- spring-framework-pet-clinic-data/src/main/java/joydeep/springframework/spring/framework/pet/clinic/models/Owner.java
+++ spring-framework-pet-clinic-data/src/main/java/joydeep/springframework/spring/framework/pet/clinic/models/Owner.java
@@ -1,14 +1,25 @@
 package joydeep.springframework.spring.framework.pet.clinic.models;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name="owners")
 public class Owner extends Person {
+
+    @Column(name="address")
     private String address;
+    @Column(name="city")
     private String city;
+    @Column(name="telephone")
     private String telephone;
 
-
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public Set<Pet> getPets() {
--- spring-framework-pet-clinic-data/src/main/java/joydeep/springframework/spring/framework/pet/clinic/models/PetType.java
+++ spring-framework-pet-clinic-data/src/main/java/joydeep/springframework/spring/framework/pet/clinic/models/PetType.java
@@ -1,6 +1,14 @@
 package joydeep.springframework.spring.framework.pet.clinic.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="types")
 public class PetType extends BaseEntity {
+
+    @Column(name="name")
     private String name;
 
     public String getName() {
