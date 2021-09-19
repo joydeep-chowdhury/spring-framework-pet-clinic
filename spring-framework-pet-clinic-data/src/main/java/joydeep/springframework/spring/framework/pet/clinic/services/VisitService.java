206f1ca0614dc6a55f9f9f0f5c6273fd520d1027
Joydeep <joydeep.chowdhury@oracle.com>
2021-09-19 16:01:11 +0530

    Create Visit Map Service closes #54

---
+++ spring-framework-pet-clinic-data/src/main/java/joydeep/springframework/spring/framework/pet/clinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package joydeep.springframework.spring.framework.pet.clinic.services;
+
+import joydeep.springframework.spring.framework.pet.clinic.models.Visit;
+
+public interface VisitService extends CrudRepository<Visit,Long>{
+}
---
+++ spring-framework-pet-clinic-data/src/main/java/joydeep/springframework/spring/framework/pet/clinic/services/map/VisitMapService.java
@@ -0,0 +1,39 @@
+package joydeep.springframework.spring.framework.pet.clinic.services.map;
+
+import joydeep.springframework.spring.framework.pet.clinic.models.Visit;
+import joydeep.springframework.spring.framework.pet.clinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Visit save(Visit entity) {
+        if (entity.getPet() == null || entity.getPet().getOwner() == null || entity.getPet().getId() == null || entity.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        } else {
+            return super.save(entity);
+        }
+    }
+
+    @Override
+    public Visit findByIdentity(Long id) {
+        return super.findByIdentity(id);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteByIdentity(Long identity) {
+        super.deleteByIdentity(identity);
+    }
+
+    @Override
+    public void delete(Visit entity) {
+        super.delete(entity);
+    }
+}
