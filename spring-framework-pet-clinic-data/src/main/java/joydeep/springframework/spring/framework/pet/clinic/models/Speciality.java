543538664846fe59fd5d71d28c6b0e2f9ca1fb42
Joydeep <joydeep.chowdhury@oracle.com>
2021-09-19 11:46:18 +0530

    Convert Vets to Jpa Entities closes #46

--- spring-framework-pet-clinic-data/src/main/java/joydeep/springframework/spring/framework/pet/clinic/models/Speciality.java
+++ spring-framework-pet-clinic-data/src/main/java/joydeep/springframework/spring/framework/pet/clinic/models/Speciality.java
@@ -1,7 +1,14 @@
 package joydeep.springframework.spring.framework.pet.clinic.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name="description")
     private String description;
 
     public String getDescription() {
