b7e982a2b00af5e22dfdad5f4b97109993d07aff
Joydeep <joydeep.chowdhury@oracle.com>
2021-09-19 11:58:37 +0530

    Create Visit entity closes #47

--- spring-framework-pet-clinic-data/src/main/java/joydeep/springframework/spring/framework/pet/clinic/models/Pet.java
+++ spring-framework-pet-clinic-data/src/main/java/joydeep/springframework/spring/framework/pet/clinic/models/Pet.java
@@ -1,11 +1,15 @@
 package joydeep.springframework.spring.framework.pet.clinic.models;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="pets")
@@ -23,6 +27,17 @@ public class Pet extends BaseEntity{
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits=new HashSet<>();
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
 
     public String getName() {
         return name;
